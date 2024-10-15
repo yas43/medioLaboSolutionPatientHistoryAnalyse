95eae5c88f78ce779c2e3f77bcaa1cceaee8e7bf
yas43 <ykeshtdar@yahoo.com>
2024-10-16 00:15:25 +0200

    initial comment

---
+++ src/main/java/com/ykeshtdar/StartP9Monolothic/StartP9MonolothicApplication.java
@@ -0,0 +1,13 @@
+package com.ykeshtdar.StartP9Monolothic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartP9MonolothicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartP9MonolothicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ykeshtdar/StartP9Monolothic/StartP9MonolothicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykeshtdar.StartP9Monolothic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartP9MonolothicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
