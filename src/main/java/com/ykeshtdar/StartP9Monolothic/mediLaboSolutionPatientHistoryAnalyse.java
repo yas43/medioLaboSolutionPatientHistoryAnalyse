8f6f78b9fd6011ff6e8778d06de655bd01cbf53e
yas43 <ykeshtdar@yahoo.com>
2024-11-06 19:40:23 +0100

    display all patient information backend and front integrate directly(without passing through gateway )

--- src/main/java/com/ykeshtdar/StartP9Monolothic/mediLaboSolutionPatientHistoryAnalyse.java
+++ src/main/java/com/ykeshtdar/StartP9Monolothic/mediLaboSolutionPatientHistoryAnalyse.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class   StartP9MonolothicApplication {
+public class mediLaboSolutionPatientHistoryAnalyse {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StartP9MonolothicApplication.class, args);
+		SpringApplication.run(mediLaboSolutionPatientHistoryAnalyse.class, args);
 	}
 
 }
